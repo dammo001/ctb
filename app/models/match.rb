280e6c7ed1b679186c790fd4c1f99557d70a4041
David Ammons <dammo001@gmail.com>
2015-11-25 23:53:18 -0800

    Player and Match models create

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,2 +1,18 @@
 class Match < ActiveRecord::Base
+	validates :winner, :loser, presence: true
+
+	belongs_to :winner,
+	class_name: "Player",
+	primary_key: :player_id,
+	foreign_key: :winner
+
+	belongs_to :loser,
+	class_name: "Player",
+	primary_key: :player_id, 
+	foreign_key: :loser 
+
+
+
+
+
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,14 @@
 class Player < ActiveRecord::Base
+	validates :name, :player_id, presence: true 
+
+	has_many :won_matches,
+	class_name: "Match",
+	primary_key: :player_id,
+	foreign_key: :winner
+
+	has_many :lost_matches,
+	class_name: "Match",
+	primary_key: :player_id,
+	foreign_key: :loser
+	
 end
